81a967fac94378b87eabc58496b76a0db5d02c30
ramyar-test <165419114+ramyar-test@users.noreply.github.com>
2025-02-06 10:28:45 +0300

    wip

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://blog-panel.suly-soft.com.www67.your-server.de/api',
+    baseURL: '//blog-panel.suly-soft.com.www67.your-server.de/api',
     headers: {
         'Content-Type': 'application/json',
         'x-key': '7368169e-b2b6-39c1-99a7-1e00804854c8',
